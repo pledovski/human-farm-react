6288ac8db223f5a628af6292d81a8c52e602b5c1
pledovski <human.pled@gmail.com>
2019-07-05 20:44:17 +0300

    initial commit

---
+++ src/components/navbar/Navbar.js
@@ -0,0 +1,15 @@
+import React, { Fragment, Component } from 'react'
+
+export default class Navbar extends Component {
+  render() {
+    return (
+      <header>
+        <div>
+          <a href="index.html">
+          <img />
+          </a>
+        </div>
+      </header>
+    )
+  }
+}
